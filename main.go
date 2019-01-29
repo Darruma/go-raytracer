8af978ebb8c4728d98267c30ca7e181efe14e5a2
darruma <umerderr@gmail.com>
2019-01-29 18:21:45 +0000

    added intersection calculations for sphere

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
-
-import ("fmt"
-        ."go-raytracer/geometry"
+import (
+	"fmt"
+	. "go-raytracer/geometry"
 )
+
 func main() {
-     var width int = 1920;
-     var height int= 1080;
-     buffer := make([]Vector3,width * height)
-     fmt.Println(buffer)
-     render()
+	var width int = 1920
+	var height int = 1080
+	buffer := make([]Vector3, width*height)
+	sp := Sphere{Vector3{3, 3, 3}, 5}
+	fmt.Println(buffer)
+	render()
 }
 
 func render() {
-  fmt.Println("Rendering")
+	fmt.Println("Rendering")
 
 }
